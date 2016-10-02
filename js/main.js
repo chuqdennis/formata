63f15d47716271d48171670a190221cf47a89b24
ChuQ Dennis <gbrjnr@gmail.com>
2016-10-02 16:46:44 +0100

    adding files for formata

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Kemis3 on 9/30/16.
+ */
+
+$('.next').click(function(){
+    var nextId = $(this).parents('.tab-pane').next().attr("id");
+    $('[href="#'+nextId+'"]').tab('show');
+    return false;
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    //update progress
+    var step = $(e.target).data('step');
+    var percent = (parseInt(step) / 5) * 100;
+
+    $('.progress-bar').css({width: percent + '%'});
+
+    //e.relatedTarget // previous tab
+});
+
+$('.submit').click(function(){
+    //$('#myWizard a:first').tab('show')
+    alert('submit form');
+    return false;
+});
